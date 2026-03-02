bfdc3165f8dfc40a6a125bf4d2b77840079c7829
PramodChamalka <shanuthpchamalka01@gmail.com>
2026-03-02 15:00:38 +0530

    feat: create post methods

--- src/main/java/com/pramod/student_management/controller/StudentController.java
+++ src/main/java/com/pramod/student_management/controller/StudentController.java
@@ -5,6 +5,7 @@ import com.pramod.student_management.repository.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -27,5 +28,11 @@ public class StudentController {
     public Student getStudent(@PathVariable int id){
         Student student = repo.findById(id).get();
         return student;
+    }
+
+    @GetMapping("/student/add")
+    public void createStudent(@RequestBody Student student){
+        repo.save(student);
+
     }
 }
